62ad03d9b71f323addc242dc20975d8cf150055b
IEEE-MAIT <63041104+ieeemait@users.noreply.github.com>
2020-04-03 20:07:51 +0530

    Update mail.js

--- js/mail.js
+++ js/mail.js
@@ -21,7 +21,7 @@ function mailsend() {
     var yyyy = today.getFullYear();
 
     today = mm + '/' + dd + '/' + yyyy;
-    var pr = fetch("http://192.168.1.5:4560/" + 'query', {
+    var pr = fetch("https://ieeemait.herokuapp.com/" + 'query', {
         method: "POST",
         body: JSON.stringify({ name: name, email: email, subject: subject, msg: msg, date: today })
     })
